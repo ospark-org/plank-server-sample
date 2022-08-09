30300cdbe4bb11b54bd5e14ad91dbe3f67e87849
Grady Zhuo <gradyzhuo@gmail.com>
2022-08-09 15:47:53 +0800

    @remove useless codes.

--- sample/__main__.py
+++ sample/__main__.py
@@ -5,16 +5,9 @@ from polymath.server.fastapi import FastAPIServer
 
 workspace = Path.cwd()
 delegate = AppDelegate()
-server = FastAPIServer.build(name="Sample", version="1.0.0", build_version="2022-08-09.000001", delegate=delegate, workspace=workspace, include_swagger=True)
-# server.swagger_settings.path = "/test"
+server = FastAPIServer.build(name="Sample", version="1.0.0", build_version="2022-08-09.000001", delegate=delegate,
+                             workspace=workspace, include_swagger=True)
 server.launch(program="debug")
 
 if __name__ == "__main__":
     uvicorn.run("sample.__main__:server", host="0.0.0.0", port=8888, workers=2)
-
-
-
-
-
-
-
